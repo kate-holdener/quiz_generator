78d22f3f2216aab8eb31650ed5ea48143c670ef8
Kate Holdener <kate.holdener@slu.edu>
2023-09-29 10:18:47 -0500

    don't reuse questions

--- generator.py
+++ generator.py
@@ -18,11 +18,19 @@ with open(args.question_file, 'r') as question_pool:
    for q in question_pool:
       all_questions.append(q)
 
+unused_indices = [*range(0, len(all_questions))]
+
 # generate required number of quizzes
 unique_indices = []
 for i in range(0, args.num_quizzes):
-   quiz_questions = random.sample(range(0, len(all_questions)), args.questions_per_quiz)
+   quiz_questions = random.sample(unused_indices, args.questions_per_quiz)
    unique_indices.append(quiz_questions)
+
+   print(unused_indices)
+
+   # remove used questions from the list
+   for index in sorted(quiz_questions, reverse=True):
+      unused_indices.remove(index)
 
 # generate quiz documents
 all_quizzes = docx.Document()
